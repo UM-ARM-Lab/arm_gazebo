b7620525ecf89abc95507aa510f9c87689dcf68f
Peter <peter@armnova>
2021-12-03 13:53:11 -0500

    move collision map plugin into this repo from link_bot_gazebo

--- arm_gazebo_plugins/src/gazebo_ros_moveit_planning_scene.cpp
+++ arm_gazebo_plugins/src/gazebo_ros_moveit_planning_scene.cpp
@@ -1,23 +1,6 @@
 /*
- * Copyright 2013 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-/*
- * Desc: A plugin which publishes the gazebo world state as a MoveIt! planning scene
- * Author: Jonathan Bohren
- * Date: 15 May 2014
+ * Publishes the gazebo world state as a MoveIt! planning scene
+ * the gazebo world frame is assumed to
  */
 
 #include <algorithm>
