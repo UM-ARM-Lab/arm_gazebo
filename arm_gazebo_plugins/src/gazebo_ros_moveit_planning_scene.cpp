UM-ARM-Lab/arm_gazebo

commit 5944b431f43cbe08adcb23d0572a520859440aba

fix bug in planning scene message
previously wasn't specifiying link pose, no idea how it was working...
but MoveIt printed a warning that I only just saw because I ran
move_group separately. Somehow it wasn't showing when it was running
from the launch file.

arm_gazebo_plugins/src/gazebo_ros_moveit_planning_scene.cpp
@@ -284,6 +284,7 @@ moveit_msgs::PlanningScene GazeboRosMoveItPlanningScene::BuildMessage()
         link_pose_msg.orientation.z = link_pose.Rot().Z();
         link_pose_msg.orientation.w = link_pose.Rot().W();
       }
+      object.pose = link_pose_msg;
 
       for (const auto &collision : collisions)
       {

commit 0b40286048dfadf05b5d6978fa5cfbe8f94ddfc9

revert bug introduced by 5944b431f43cbe08adcb23d0572a520859440aba
undo using link_pose_msg, collision_object_pose is already in world frame

arm_gazebo_plugins/src/gazebo_ros_moveit_planning_scene.cpp
@@ -256,19 +256,6 @@ moveit_msgs::PlanningScene GazeboRosMoveItPlanningScene::BuildMessage()
       object.header.stamp = ros::Time::now();
       object.operation = moveit_msgs::CollisionObject::ADD;
 
-      ignition::math::Pose3d link_pose = link->WorldPose();
-      geometry_msgs::Pose link_pose_msg;
-      {
-        link_pose_msg.position.x = link_pose.Pos().X();
-        link_pose_msg.position.y = link_pose.Pos().Y();
-        link_pose_msg.position.z = link_pose.Pos().Z();
-        link_pose_msg.orientation.x = link_pose.Rot().X();
-        link_pose_msg.orientation.y = link_pose.Rot().Y();
-        link_pose_msg.orientation.z = link_pose.Rot().Z();
-        link_pose_msg.orientation.w = link_pose.Rot().W();
-      }
-      object.pose = link_pose_msg;
-
       for (const auto &collision : collisions)
       {
         const ShapePtr shape = collision->GetShape();